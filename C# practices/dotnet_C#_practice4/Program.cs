89a4ad96d906f2860e7c9db5131bb0d1a0cd4b12
SeyidAhmetYogurtcu <SeyidAhmetYogurtcu@gmail.com>
2022-03-09 01:16:21 +0300

    Update
    Pratice4 added

---
+++ C# practices/dotnet_C#_practice4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace dotnet_C__practice4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.WriteLine("bir sayı girin:");
+            int sayi =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("bir sayı girin::"+sayi);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata: "+ex.Message.ToString());
+            }
+            // finally
+            // {
+            //     Console.WriteLine("İşlem Tamamlandı!");
+            // }
+            try
+            {
+               // int a=int.Parse(null);    
+                //int a=int.Parse("test");    
+                int a=int.Parse("-12345678910");    
+
+
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük veye çok büyük bir değer girdiniz");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem Başarıyla Tamamlandı!");
+            }
+        }
+    }
+}
