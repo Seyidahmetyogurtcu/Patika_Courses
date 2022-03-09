8e712347b2a8a04348aa96e45db83056bd447bdf
SeyidAhmetYogurtcu <SeyidAhmetYogurtcu@gmail.com>
2022-03-09 10:26:49 +0300

    Update
    if else ternary

---
+++ C# practices/dotnet_C#_practice5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace dotnet_C__practice5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int time=DateTime.Now.Hour;
+                Console.WriteLine(time);
+            if(time>=6&&time<11)
+            {
+                Console.WriteLine("Günaydın !");
+            }
+            else if(time<=18)
+            {
+                Console.WriteLine("İyi günler !");
+            }
+            else
+            {
+                 Console.WriteLine("İyi geceler !");  
+            }
+
+            string sonuç = time<=18? "İyi günler !":"İyi geceler !";
+            sonuç= time>=6 && time<11 ? "Günaydın": time<=18 ? "İyi günler !":"İyi geceler !";
+            Console.WriteLine(sonuç);  
+        }
+    }
+}
