c995647623527597d03bb54542a33a76d3c3c429
SeyidAhmetYogurtcu <SeyidAhmetYogurtcu@gmail.com>
2022-03-09 17:49:29 +0300

    Update
    pratik 8

---
+++ C# practices/dotnet_C#_practice8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace dotnet_C__practice8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen sayı girin: ");
+            int sayi =int.Parse(Console.ReadLine());
+            int sayac=1;
+            int toplam=0;
+            while(sayac<=sayi)
+            {
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            char character='a';
+            while (character<'z')
+            {
+                Console.WriteLine(character);
+                character++;
+            }
+
+            string[] arabalar={"BMV","Mercedes","Ford"};
+            foreach(var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
