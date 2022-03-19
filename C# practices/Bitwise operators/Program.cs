5f3cc5ede85038b4675d3d9b714c7a403d131b0c
SeyidAhmetYogurtcu <SeyidAhmetYogurtcu@gmail.com>
2022-03-19 11:12:57 +0300

    Update
    pratik 17 addaed.
    bitwise operators added.

---
+++ C# practices/Bitwise operators/Program.cs
@@ -0,0 +1,46 @@
+﻿// C# program to demonstrate the working
+// of Bitwise Operators
+using System;
+namespace Bitwise {
+	
+    class GFG {
+        
+        // Main Function
+        static void Main(string[] args)
+        {
+            int x = 5, y = 10, result;//5:0101 ,  10:1010
+            
+                // Bitwise AND Operator
+                result = x & y; //0000
+                Console.WriteLine("Bitwise AND: " + result);
+                
+                // Bitwise OR Operator
+                result = x | y;//1111
+                Console.WriteLine("Bitwise OR: " + result);
+                
+                // Bitwise XOR Operator
+                result = x ^ y;//1111
+                Console.WriteLine("Bitwise XOR: " + result);
+                
+                // Bitwise AND Operator
+                result = ~x;//0110   2's complement
+                Console.WriteLine("Bitwise Complement: " + result);
+                
+                // Bitwise LEFT SHIFT Operator
+                result = x << 2;//10100
+                Console.WriteLine("Bitwise Left Shift: " + result);
+                
+                // Bitwise RIGHT SHIFT Operator
+                result = x >> 2;//1
+                Console.WriteLine("Bitwise Right Shift: " + result);
+            
+
+            /*Bitwise AND: 0
+            Bitwise OR: 15
+            Bitwise XOR: 15
+            Bitwise Complement: -6
+            Bitwise Left Shift: 20
+            Bitwise Right Shift: 1*/
+        }
+    }
+}
---
+++ C# practices/dotnet_C#_practice17/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace dotnet_C__practice17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             ArrayList arrayList = new();
+            // arrayList.Add("Ali");
+            // arrayList.Add(2);
+            // arrayList.Add(true);
+            // arrayList.Add('a');
+
+            // //içerisindeki verilere erişim
+            // foreach (var item in arrayList)
+            //     System.Console.WriteLine(item);
+            
+
+            //AddRange
+            System.Console.WriteLine("******* Add Range ********");
+            //string[] renkler = {"kırmızı","sarı","yeşil"};
+            List<int> sayilar =new List<int>{1,8,3,7,9,92,5};
+            //arrayList.AddRange(renkler);
+            arrayList.AddRange(sayilar);
+            foreach (var item in arrayList)
+                System.Console.WriteLine(item);
+
+            //Sort
+            System.Console.WriteLine("******* Sort ********");
+            arrayList.Sort(); //to sort you need to have same type in it
+             foreach (var item in arrayList)
+                System.Console.WriteLine(item);
+
+            System.Console.WriteLine("*********  Binary Seach **********");
+            //Binary Search
+            System.Console.WriteLine(arrayList.BinarySearch(1));
+            System.Console.WriteLine(arrayList.BinarySearch(3));
+            System.Console.WriteLine(arrayList.BinarySearch(5));
+            System.Console.WriteLine(arrayList.BinarySearch(7));
+            System.Console.WriteLine(arrayList.BinarySearch(8));
+            System.Console.WriteLine(arrayList.BinarySearch(9));//9 un indexini getiriyor(zero based index:indexler sıfırıncı indexten başlıyor).sort ettikten sonraki hali ile.
+            System.Console.WriteLine(arrayList.BinarySearch(92));
+
+
+            //Reverse
+            System.Console.WriteLine("*********   Reverse **********");
+            arrayList.Reverse();
+              foreach (var item in arrayList)
+                System.Console.WriteLine(item);
+
+            //Clear
+            System.Console.WriteLine("*********   Reverse **********");
+            arrayList.Clear();
+            foreach (var item in arrayList)
+                System.Console.WriteLine(item);
+        }
+    }
+}
